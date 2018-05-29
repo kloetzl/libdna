0b9e56be0bde22b86ef288b128749871b72d6db2
Fabian Klötzl <fabian@kloetzl.info>
2018-05-29 16:20:18 +0200

    whoops

--- src/dna4_hash.c
+++ src/dna4_hash.c
@@ -10,7 +10,7 @@ uint64_t dna4_hash(const char *begin, size_t k)
 	uint64_t res = 0;
 	for (size_t i = 0; i < k; i++) {
 		res <<= 2;
-		char c = (begin[k] & 6) >> 1;
+		char c = (begin[i] & 6) >> 1;
 		res |= c ^ (c >> 1);
 	}
 	return res;
