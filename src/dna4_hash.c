bacc7e9194c34d49b859127157f5b3aa24dc349e
Fabian Klötzl <fabian@kloetzl.info>
2018-06-01 16:38:09 +0200

    improve dna4_hash

--- src/dna4_hash.c
+++ src/dna4_hash.c
@@ -5,13 +5,32 @@
 
 #include "dna.h"
 
+/**
+ * Somewhat surprisingly, using a table is faster than twiddling. I am guassing
+ * because of the smaller number of instructions.
+ */
+
 uint64_t dna4_hash(const char *begin, size_t k)
 {
+	const unsigned char *ubegin = (const unsigned char *)begin;
+
 	uint64_t res = 0;
 	for (size_t i = 0; i < k; i++) {
+		char c = dnax_hash_table[ubegin[i]];
 		res <<= 2;
-		char c = (begin[i] & 6) >> 1;
-		res |= c ^ (c >> 1);
+		res |= c;
 	}
+
 	return res;
 }
+
+// uint64_t dna4_hash_direct(const char *begin, size_t k)
+// {
+// 	uint64_t res = 0;
+// 	for (size_t i = 0; i < k; i++) {
+// 		res <<= 2;
+// 		char c = (begin[i] & 6) >> 1;
+// 		res |= c ^ (c >> 1);
+// 	}
+// 	return res;
+// }
