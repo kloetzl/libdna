f08dbc006b08b410af654299cc94c01b86231fb6
Fabian Klötzl <fabian@kloetzl.info>
2019-08-09 16:42:45 +0200

    Tdnax_revcomp: check edge cases

--- test/Tdnax_revcomp.cxx
+++ test/Tdnax_revcomp.cxx
@@ -38,3 +38,68 @@ TEST_CASE("Basic revcomp checks")
 
 	delete[] buffer;
 }
+
+TEST_CASE("Mono characters")
+{
+	auto forward = std::string(100, 'A');
+	auto reverse = std::string(100, 0);
+
+	dnax_revcomp(
+		dnax_revcomp_table, dna::begin(forward), dna::end(forward),
+		dna::begin(reverse));
+	REQUIRE(std::string(100, 'T') == reverse);
+
+	forward = std::string(100, 'T');
+	reverse = std::string(100, 0);
+
+	dnax_revcomp(
+		dnax_revcomp_table, dna::begin(forward), dna::end(forward),
+		dna::begin(reverse));
+	REQUIRE(std::string(100, 'A') == reverse);
+
+	forward = std::string(100, 'C');
+	reverse = std::string(100, 0);
+
+	dnax_revcomp(
+		dnax_revcomp_table, dna::begin(forward), dna::end(forward),
+		dna::begin(reverse));
+	REQUIRE(std::string(100, 'G') == reverse);
+
+	forward = std::string(100, 'G');
+	reverse = std::string(100, 0);
+
+	dnax_revcomp(
+		dnax_revcomp_table, dna::begin(forward), dna::end(forward),
+		dna::begin(reverse));
+	REQUIRE(std::string(100, 'C') == reverse);
+}
+
+namespace dnax
+{
+auto
+revcomp(const std::string &forward)
+{
+	auto reverse = std::string(forward.size(), 0);
+	auto end = dnax_revcomp(
+		dnax_revcomp_table, dna::begin(forward), dna::end(forward),
+		dna::begin(reverse));
+	reverse.erase(end - dna::begin(reverse));
+	return reverse;
+}
+} // namespace dnax
+
+using namespace std::string_literals;
+
+TEST_CASE("Edge cases")
+{
+	REQUIRE(dnax::revcomp("") == ""s);
+	REQUIRE(dnax::revcomp("T") == "A"s);
+	REQUIRE(dnax::revcomp("G") == "C"s);
+	REQUIRE(dnax::revcomp("C") == "G"s);
+	REQUIRE(dnax::revcomp("A") == "T"s);
+	REQUIRE(dnax::revcomp("t") == "a"s);
+	REQUIRE(dnax::revcomp("g") == "c"s);
+	REQUIRE(dnax::revcomp("c") == "g"s);
+	REQUIRE(dnax::revcomp("a") == "t"s);
+	REQUIRE(dnax::revcomp("_") == ""s);
+}
