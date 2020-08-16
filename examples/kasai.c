056ddafce397408414861dd4bef9c5e9c1e52eef
Fabian Klötzl <fabian@kloetzl.info>
2020-08-16 11:44:47 +0200

    examples: add kasai

---
+++ examples/kasai.c
@@ -0,0 +1,20 @@
+void kasai (const char *T, const int *SA, const int *ISA, size_t n, int *LCP)
+{
+	int k = 0;
+	LCP[0] = -1;
+	LCP[n] = -1;
+
+	int l = 0;
+
+	for (int i = 0; i < n; i++) {
+		int j = ISA[i];
+		if (j <= 0) continue;
+
+		k = SA[j - 1];
+		char *ptr = dna4_first_mismatch(T + k + l, T + i + l);
+		l = ptr - T - k - l;
+
+		LCP[j] = l;
+		l = l ? l - 1 : 0;
+	}
+}
