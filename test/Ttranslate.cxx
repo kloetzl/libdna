02df457ca1052de75fc51c2ef3c6fb0396a13d84
Fabian Klötzl <fabian@kloetzl.info>
2020-08-17 11:52:01 +0200

    formatting

--- test/Ttranslate.cxx
+++ test/Ttranslate.cxx
@@ -9,8 +9,7 @@ TEST_CASE("Some simple checks")
 	auto mrna = std::string("AAA-AAC-AAG-AAT-ACAACCACGACT");
 	char *aa = new char[mrna.size() / 3 + 10];
 
-	char *ptr =
-		dnax_translate(mrna.data(), mrna.data() + mrna.size(), aa);
+	char *ptr = dnax_translate(mrna.data(), mrna.data() + mrna.size(), aa);
 	*ptr = 0;
 
 	REQUIRE(std::string(aa) == "KNKNTTTT");
