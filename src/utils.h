29ec48f08add2c25342d721cd9cc9d8cc36f8999
Fabian Klötzl <fabian@kloetzl.info>
2020-12-26 11:10:55 +0100

    bump copyright year

--- src/utils.h
+++ src/utils.h
@@ -1,12 +1,9 @@
 /**
  * SPDX-License-Identifier: GPL-3.0-or-later
- * Copyright 2018 (C) Fabian Klötzl
+ * Copyright 2018 - 2020 (C) Fabian Klötzl
  */
 
 #pragma once
-
-double
-dna_utils_log(double);
 
 #define UNLIKELY(X) __builtin_expect((X), 0)
 #define LIKELY(X) __builtin_expect((X), 1)
