b9c7c372032561e48f94e37b373721ecb0d93c9b
Fabian Klötzl <fabian@kloetzl.info>
2021-04-05 19:00:07 +0100

    dna4_revcomp: speed up implementation for x86
    closes #11

--- src/dna4_revcomp_avx2.c
+++ src/dna4_revcomp_avx2.c
@@ -30,32 +30,25 @@ dna4_revcomp_avx2(const char *begin, const char *end, char *restrict dest)
 	size_t vec_offset = 0;
 	size_t vec_length = length / vec_bytes;
 
+	vec_type mask_shuffle = _mm256_set_epi8(
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5,
+		6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+	vec_type nibblecode = _mm256_setr_epi8(
+		0, 'T', 0, 'G', 'A', 0, 0, 'C', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'T', 0, 'G',
+		'A', 0, 0, 'C', 0, 0, 0, 0, 0, 0, 0, 0);
+
 	for (; vec_offset < vec_length; vec_offset++) {
 		vec_type chunk;
 		memcpy(&chunk, end - (vec_offset + 1) * vec_bytes, vec_bytes);
 
-		// reverse
-		vec_type mask_shuffle = _mm256_set_epi8(
-			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4,
-			5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 		vec_type almostreversed = _mm256_shuffle_epi8(chunk, mask_shuffle);
 		vec_type reversed =
 			_mm256_permute2x128_si256(almostreversed, almostreversed, 1);
 
-		vec_type all_bytes_0 = _mm256_set1_epi8(0);
-		vec_type all_bytes_2 = _mm256_set1_epi8(2);
-		vec_type all_bytes_4 = _mm256_set1_epi8(4);
-		vec_type all_bytes_21 = _mm256_set1_epi8(21);
+		vec_type mapped = _mm256_shuffle_epi8(nibblecode, reversed);
 
-		// cmp
-		vec_type anded = _mm256_and_si256(reversed, all_bytes_2);
-		vec_type iszero = _mm256_cmpeq_epi8(anded, all_bytes_0);
-
-		vec_type xor_mask =
-			_mm256_blendv_epi8(all_bytes_4, all_bytes_21, iszero);
-		vec_type comp = _mm256_xor_si256(xor_mask, reversed);
-
-		memcpy(dest + vec_offset * vec_bytes, &comp, vec_bytes);
+		memcpy(dest + vec_offset * vec_bytes, &mapped, vec_bytes);
 	}
 
 	for (size_t i = 0; i < length - vec_offset * vec_bytes; i++) {
--- src/dna4_revcomp_sse42.c
+++ src/dna4_revcomp_sse42.c
@@ -29,36 +29,20 @@ dna4_revcomp_sse42(const char *begin, const char *end, char *restrict dest)
 	size_t vec_offset = 0;
 	size_t vec_length = length / vec_bytes;
 
+	vec_type nibblecode =
+		_mm_setr_epi8(0, 'T', 0, 'G', 'A', 0, 0, 'C', 0, 0, 0, 0, 0, 0, 0, 0);
+	vec_type mask_shuffle =
+		_mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
 	for (; vec_offset < vec_length; vec_offset++) {
 		vec_type chunk;
 		memcpy(&chunk, end - (vec_offset + 1) * vec_bytes, vec_bytes);
 
-		// reverse
-		vec_type mask_shuffle =
-			_mm_set_epi8(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 		vec_type reversed = _mm_shuffle_epi8(chunk, mask_shuffle);
-
-		vec_type all_bytes_0 = _mm_set1_epi8(0);
-		vec_type all_bytes_2 = _mm_set1_epi8(2);
-		vec_type all_bytes_4 = _mm_set1_epi8(4);
-		vec_type all_bytes_21 = _mm_set1_epi8(21);
-
-		// cmp
-		vec_type anded = _mm_and_si128(reversed, all_bytes_2);
-		vec_type iszero = _mm_cmpeq_epi8(anded, all_bytes_0);
-
-		// SSE4.2
-		vec_type xor_mask = _mm_blendv_epi8(all_bytes_4, all_bytes_21, iszero);
-
-		// SSSE3
-		// vec_type is_AorT = _mm_and_si128(all_bytes_4, iszero);
-		// vec_type is_CorG = _mm_andnot_si128(iszero, all_bytes_21);
-		// vec_type xor_mask = _mm_or_si128(is_AorT, is_CorG);
-
-		vec_type comp = _mm_xor_si128(xor_mask, reversed);
+		vec_type mapped = _mm_shuffle_epi8(nibblecode, reversed);
 
 		size_t offset = vec_offset * vec_bytes;
-		memcpy(dest + offset, &comp, vec_bytes);
+		memcpy(dest + offset, &mapped, vec_bytes);
 	}
 
 	for (size_t i = 0; i < length - vec_offset * vec_bytes; i++) {
