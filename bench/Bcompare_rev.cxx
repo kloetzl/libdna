77605e36e87bf0f1077e62bc74e7aa512f297aab
Fabian Klötzl <fabian@kloetzl.info>
2021-10-02 22:02:27 +0100

    bench: add smart compare_rev

--- bench/Bcompare_rev.cxx
+++ bench/Bcompare_rev.cxx
@@ -160,6 +160,56 @@ xoreq_avx2(const char *begin, const char *end, const char *other)
 
 	return substitutions;
 }
+
+size_t
+shuffle_avx2(const char *begin, const char *end, const char *other)
+{
+	size_t length = end - begin;
+	size_t substitutions = 0;
+	size_t offset = 0;
+
+	typedef __m256i vec_type;
+	size_t vec_size = sizeof(vec_type);
+	size_t vec_offset = 0;
+	size_t vec_length = length / vec_size;
+
+	substitutions += vec_size * vec_length;
+
+	vec_type nibblecode = _mm256_setr_epi8(
+		'0', 'T', '2', 'G', 'A', '5', '6', 'C', '8', '9', 'a', 'b', 'c', 'd',
+		'e', 'f', '0', 'T', '2', 'G', 'A', '5', '6', 'C', '8', '9', 'a', 'b',
+		'c', 'd', 'e', 'f');
+	vec_type mask = _mm256_set_epi8(
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0, 1, 2, 3, 4, 5,
+		6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+
+	for (; vec_offset < vec_length; vec_offset++) {
+		vec_type b;
+		memcpy(&b, begin + vec_offset * vec_size, vec_size);
+		vec_type o;
+		size_t pos = length - (vec_offset + 1) * vec_size;
+		memcpy(&o, other + pos, vec_size);
+
+		vec_type reversed = _mm256_shuffle_epi8(o, mask);
+		vec_type revcomped = _mm256_shuffle_epi8(nibblecode, reversed);
+		vec_type swapped = _mm256_permute2x128_si256(b, b, 1);
+
+		vec_type v3 = _mm256_cmpeq_epi8(revcomped, swapped);
+
+		unsigned int vmask = _mm256_movemask_epi8(v3);
+		substitutions -= __builtin_popcount(vmask);
+	}
+
+	offset += vec_offset * vec_size;
+
+	for (; offset < length; offset++) {
+		if (!is_complement(begin[offset], other[length - 1 - offset])) {
+			substitutions++;
+		}
+	}
+
+	return substitutions;
+}
 #endif
 
 #ifdef __SSE4_2__
@@ -279,6 +329,7 @@ BENCHMARK_CAPTURE(bench, dna4_count_mismatches, dna4_count_mismatches);
 
 #ifdef __AVX2__
 BENCHMARK_CAPTURE(bench, xoreq_avx2, xoreq_avx2);
+BENCHMARK_CAPTURE(bench, shuffle_avx2, shuffle_avx2);
 #endif
 
 #ifdef __SSE4_2__
