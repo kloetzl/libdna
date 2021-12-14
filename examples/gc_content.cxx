d3a0ce6ff0d57bee78b4fb9c747c5fc91d7d36d2
Fabian Klötzl <fabian@kloetzl.info>
2021-12-14 18:09:29 +0100

    fix typo

--- examples/gc_content.cxx
+++ examples/gc_content.cxx
@@ -3,7 +3,7 @@
 double
 gc_content(const std::string &str)
 {
-	auto counts = dna::count(str);
+	auto counts = dnax::count(str);
 	auto gc = counts['G'] + counts['C'] + counts['S'] + counts['g'] +
 			  counts['c'] + counts['s'];
 	return (double)gc / str.size();
