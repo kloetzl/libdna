904e12b311180b33fb11f2a0ffacf2a58f4293d6
Fabian Klötzl <fabian@kloetzl.info>
2022-01-03 19:15:15 +0100

    test dna4_count_mismatches: test native implementations

--- test/Tcompare.cxx
+++ test/Tcompare.cxx
@@ -7,6 +7,28 @@
 #include <string.h>
 #include <string>
 
+namespace dna4
+{
+using function_type = size_t(const char *, const char *, const char *);
+
+template <function_type fn>
+size_t
+cm(std::string_view s1, std::string_view s2)
+{
+	return fn(dna::begin(s1), dna::end(s1), dna::begin(s2));
+}
+
+#ifdef EXPOSE_INTERNALS
+
+const auto count_mismatches_generic = cm<dna4_count_mismatches_generic>;
+const auto count_mismatches_sse2 = cm<dna4_count_mismatches_sse2>;
+const auto count_mismatches_avx2 = cm<dna4_count_mismatches_avx2>;
+const auto count_mismatches_avx512 = cm<dna4_count_mismatches_avx512>;
+
+#endif
+
+} // namespace dna4
+
 using namespace std::string_literals;
 
 TEST_CASE("Some simple checks")
@@ -47,3 +69,68 @@ TEST_CASE("Long strings")
 		dna::begin(longsubject), dna::end(longsubject), dna::begin(longquery));
 	REQUIRE(mismatches == (1 * multiplier));
 }
+
+#ifdef EXPOSE_INTERNALS
+
+TEST_CASE("Various Generic")
+{
+	const auto subject = "AACGTACGT"s;
+	const auto query = "AACGTACCT"s;
+
+	const auto multipliers = {1, 2, 3, 4, 10, 20, 100};
+	for (size_t multiplier : multipliers) {
+		const auto longsubject = repeat(subject, multiplier);
+		const auto longquery = repeat(query, multiplier);
+		REQUIRE(
+			dna4::count_mismatches_generic(longsubject, longquery) ==
+			multiplier);
+	}
+}
+
+#ifdef __SSE4_2__
+TEST_CASE("Various SSE42")
+{
+	const auto subject = "AACGTACGT"s;
+	const auto query = "AACGTACCT"s;
+
+	const auto multipliers = {1, 2, 3, 4, 10, 20, 100};
+	for (size_t multiplier : multipliers) {
+		const auto longsubject = repeat(subject, multiplier);
+		const auto longquery = repeat(query, multiplier);
+		REQUIRE(
+			dna4::count_mismatches_sse2(longsubject, longquery) == multiplier);
+	}
+}
+#endif
+
+#ifdef __AVX2__
+TEST_CASE("Various AVX2")
+{
+	const auto subject = "AACGTACGT"s;
+	const auto query = "AACGTACCT"s;
+
+	const auto multipliers = {1, 2, 3, 4, 10, 20, 100};
+	for (size_t multiplier : multipliers) {
+		const auto longsubject = repeat(subject, multiplier);
+		const auto longquery = repeat(query, multiplier);
+		REQUIRE(
+			dna4::count_mismatches_avx2(longsubject, longquery) == multiplier);
+	}
+}
+#endif
+
+TEST_CASE("Dispatch")
+{
+	// The test should be compiled and executed with -march=native.
+#ifdef __AVX2__
+	REQUIRE(&dna4_count_mismatches == &dna4_count_mismatches_avx2);
+#else
+#ifdef __SSE4_2__
+	REQUIRE(&dna4_count_mismatches == &dna4_count_mismatches_sse2);
+#else
+	REQUIRE(&dna4_count_mismatches == &dna4_count_mismatches_generic);
+#endif
+#endif
+}
+
+#endif
