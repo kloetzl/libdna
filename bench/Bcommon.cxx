560c1ac7526f8e23e9b6628414146bc4c9b775c5
Fabian Klötzl <fabian@kloetzl.info>
2022-04-10 21:48:16 +0100

    bench: use dna4_fill_random for test case generation

--- bench/Bcommon.cxx
+++ bench/Bcommon.cxx
@@ -1,3 +1,4 @@
+#include <dna.h>
 #include <random>
 
 extern size_t seed;
@@ -6,16 +7,7 @@ extern size_t invrate;
 void
 gen(char *str, size_t length)
 {
-	static const char *ACGT = "ACGT";
-	auto base_rand = std::default_random_engine{
-		static_cast<std::default_random_engine::result_type>(seed)};
-	auto base_dist = std::uniform_int_distribution<int>{0, 3};
-	auto base_acgt = [&] { return ACGT[base_dist(base_rand)]; };
-
-	while (length--) {
-		*str++ = base_acgt();
-	}
-
+	dna4_fill_random(str, str + length, seed);
 	*str = '\0';
 }
 
