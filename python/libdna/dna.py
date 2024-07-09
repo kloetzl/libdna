47285a1843830f2f129674b17bbaa072f24cc4a4
Fabian Klötzl <fabian@kloetzl.info>
2024-07-09 17:02:37 +0100

    python: format

--- python/libdna/dna.py
+++ python/libdna/dna.py
@@ -1,62 +1,61 @@
 # SPDX-License-Identifier: MIT
 # Copyright 2024 (C) Fabian Klötzl
 
+
 def version() -> int:
-	return 0
+    return 0
 
 
 def _uint64(value: int) -> int:
-	return value & 0xffffffffFFFFFFFF
+    return value & 0xFFFFFFFFFFFFFFFF
 
 
 def ihash(value: int) -> int:
-	key = _uint64(value)
-	key = _uint64(key * 0x7b215eedec9e1967)
-	key ^= key >> 37
-	key = _uint64(key + 0x16c5c874ea637686)
-	key ^= _uint64(key << 11)
-	key = _uint64(key * 0xff7dbf225491d985)
-	key ^= key >> 15
-	key = _uint64(key * 0x3d5fe41de2ea4e4f)
-	key ^= key >> 19
-	key = _uint64(key + 0x44ee2cfcca48954e)
-	key ^= key >> 23
-	key = _uint64(key * 0x4f7659e92097460b)
-	key ^= key >> 29
-	return key
+    key = _uint64(value)
+    key = _uint64(key * 0x7B215EEDEC9E1967)
+    key ^= key >> 37
+    key = _uint64(key + 0x16C5C874EA637686)
+    key ^= _uint64(key << 11)
+    key = _uint64(key * 0xFF7DBF225491D985)
+    key ^= key >> 15
+    key = _uint64(key * 0x3D5FE41DE2EA4E4F)
+    key ^= key >> 19
+    key = _uint64(key + 0x44EE2CFCCA48954E)
+    key ^= key >> 23
+    key = _uint64(key * 0x4F7659E92097460B)
+    key ^= key >> 29
+    return key
 
 
 def ihash_invert(value: int) -> int:
-	key = value
-	tmp = key
-	tmp = key ^ key >> 29
-	tmp = key ^ tmp >> 29
-	key = key ^ tmp >> 29
-	key = _uint64(key * 0x66e53f4c3ffe95a3)
-	tmp = key ^ key >> 23
-	tmp = key ^ tmp >> 23
-	key = key ^ tmp >> 23
-	key = _uint64(key - 0x44ee2cfcca48954e)
-	tmp = key ^ key >> 19
-	tmp = key ^ tmp >> 19
-	tmp = key ^ tmp >> 19
-	key = key ^ tmp >> 19
-	key = _uint64(key * 0xf304917f13fe08af)
-	tmp = key ^ key >> 15
-	tmp = key ^ tmp >> 15
-	tmp = key ^ tmp >> 15
-	key = key ^ tmp >> 15
-	key = _uint64(key * 0x398b71be7aae374d)
-	tmp = key ^ key << 11
-	tmp = key ^ tmp << 11
-	tmp = key ^ tmp << 11
-	tmp = key ^ tmp << 11
-	tmp = key ^ tmp << 11
-	tmp = key ^ tmp << 11
-	key = key ^ tmp << 11
-	key = _uint64(key - 0x16c5c874ea637686)
-	key ^= key >> 37
-	key = _uint64(key * 0xee84443b086ef257)
-	return key
-
-
+    key = value
+    tmp = key
+    tmp = key ^ key >> 29
+    tmp = key ^ tmp >> 29
+    key = key ^ tmp >> 29
+    key = _uint64(key * 0x66E53F4C3FFE95A3)
+    tmp = key ^ key >> 23
+    tmp = key ^ tmp >> 23
+    key = key ^ tmp >> 23
+    key = _uint64(key - 0x44EE2CFCCA48954E)
+    tmp = key ^ key >> 19
+    tmp = key ^ tmp >> 19
+    tmp = key ^ tmp >> 19
+    key = key ^ tmp >> 19
+    key = _uint64(key * 0xF304917F13FE08AF)
+    tmp = key ^ key >> 15
+    tmp = key ^ tmp >> 15
+    tmp = key ^ tmp >> 15
+    key = key ^ tmp >> 15
+    key = _uint64(key * 0x398B71BE7AAE374D)
+    tmp = key ^ key << 11
+    tmp = key ^ tmp << 11
+    tmp = key ^ tmp << 11
+    tmp = key ^ tmp << 11
+    tmp = key ^ tmp << 11
+    tmp = key ^ tmp << 11
+    key = key ^ tmp << 11
+    key = _uint64(key - 0x16C5C874EA637686)
+    key ^= key >> 37
+    key = _uint64(key * 0xEE84443B086EF257)
+    return key
--- python/libdna/test_dnax.py
+++ python/libdna/test_dnax.py
@@ -5,34 +5,30 @@ from dnax import *
 
 
 def test_simple():
-	assert count_mismatches("AACGT", "AACGT") == 0
-	assert extract_dna4("acgt") == "ACGT"
-	assert find_first_mismatch("ACGT", "ACGt") == 3
-	assert find_first_of("ACGTt!", "acgt") == 4
-	assert find_first_not_of("ACGTt!", "acgt") == 0
-	assert find_first_not_of("ACGT", set("ACGT")) == 4
+    assert count_mismatches("AACGT", "AACGT") == 0
+    assert extract_dna4("acgt") == "ACGT"
+    assert find_first_mismatch("ACGT", "ACGt") == 3
+    assert find_first_of("ACGTt!", "acgt") == 4
+    assert find_first_not_of("ACGTt!", "acgt") == 0
+    assert find_first_not_of("ACGT", set("ACGT")) == 4
 
-	a = "AACGTACGT"
-	b = "AACGTACCT"
-	assert count_mismatches(a, b) == 1
-	aa = revcomp(a)
-	bb = revcomp(b)
-	assert count_mismatches(aa, bb) == 1
+    a = "AACGTACGT"
+    b = "AACGTACCT"
+    assert count_mismatches(a, b) == 1
+    aa = revcomp(a)
+    bb = revcomp(b)
+    assert count_mismatches(aa, bb) == 1
 
 
 def test_count_mismatches():
-	seq1 = "ACGTACGTACGT"
-	seq2 = "ACGTTCGTACGA"
-	assert count_mismatches(seq1, seq2) == 2
+    seq1 = "ACGTACGTACGT"
+    seq2 = "ACGTTCGTACGA"
+    assert count_mismatches(seq1, seq2) == 2
 
 
 def test_revcomp():
-	seq = "ACGTACGTACGT"
-	assert revcomp(seq) == seq
+    seq = "ACGTACGTACGT"
+    assert revcomp(seq) == seq
 
-	seq = "ACGTacgtACGT!"
-	assert revcomp(seq) == "ACGTacgtACGT"
-
-
-
-
+    seq = "ACGTacgtACGT!"
+    assert revcomp(seq) == "ACGTacgtACGT"
